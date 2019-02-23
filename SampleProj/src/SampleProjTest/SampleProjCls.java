37e822d819d36942ed844eb2e43d11a34ad25cbb
Lalit <Lalit@lalitsur.in.ibm.com>
2019-02-23 17:43:15 +0530

    added Class

--- SampleProj/src/SampleProjTest/SampleProjCls.java
+++ SampleProj/src/SampleProjTest/SampleProjCls.java
@@ -5,6 +5,7 @@ public class SampleProjCls {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Login Github");
+			System.out.println("Committed");
 	}
 
 }
