dd9d88595fd9d9f3815f06b3c7adf1b831a4a31a
Lalit <Lalit@lalitsur.in.ibm.com>
2019-02-23 17:39:34 +0530

    Added new pkg and class

---
+++ SampleProj/src/SampleProjTest/SampleProjCls.java
@@ -0,0 +1,10 @@
+package SampleProjTest;
+
+public class SampleProjCls {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Login Github");
+	}
+
+}
